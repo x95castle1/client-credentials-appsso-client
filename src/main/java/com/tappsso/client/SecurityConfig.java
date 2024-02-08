3d6f58321b910d0dfe695e8d86c5538cefc230ee
Jeremy Castle <x95castle1@yahoo.com>
2024-02-08 17:07:57 -0600

    Stuff

--- src/main/java/com/tappsso/client/SecurityConfig.java
+++ src/main/java/com/tappsso/client/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-
-import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
