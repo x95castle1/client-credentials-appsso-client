ab7d1836862c8cd85916847799c49aa601553f51
Jeremy Castle <x95castle1@yahoo.com>
2024-02-08 15:39:06 -0600

    Added property loggers

--- src/main/java/com/tappsso/client/DemoApplication.java
+++ src/main/java/com/tappsso/client/DemoApplication.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Configuration;
 public class DemoApplication  {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(DemoApplication.class);
+		springApplication.addListeners(new PropertiesLogger());
+		springApplication.run(args);
 	}
 
 }
